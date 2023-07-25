65e5408c58328be01679812c8126ce46a8b2a905
admin <pradip@adstringo.in>
2023-07-25 17:25:37 +0530

    25 july

--- lib/upload_image.dart
+++ lib/upload_image.dart
@@ -4,9 +4,11 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:http/http.dart' as http;
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:multi_image_picker/multi_image_picker.dart';
+import 'package:multipart/model.dart';
 import 'package:path_provider/path_provider.dart';
+
+import 'comModel.dart';
 
 class UploadImageScreen extends StatefulWidget {
   const UploadImageScreen({Key? key}) : super(key: key);
@@ -16,109 +18,155 @@ class UploadImageScreen extends StatefulWidget {
 }
 
 class _UploadImageScreenState extends State<UploadImageScreen> {
-  var BCompresspath = '';
-  var ACompresspath = '';
+  String imageSize = 'Calculating...';
+
   late String imgName;
   List<Asset> imagesList = <Asset>[];
+  List<Asset> resultList = <Asset>[];
+  List<RandomData> myImagesList = <RandomData>[];
+  List<CompRandomData> myComImagesList = <CompRandomData>[];
   List<File> compressedImages = [];
 
-  int height = 0;
-  int width = 0;
-  double sizeInKilobytes1 = 0.0;
-  double sizeInKilobytes = 0.0;
   String _error = 'No Error Dectected';
-  var image3;
-  var img4;
-
+  double imageSizekb = 0.0;
   bool showSpinner = false;
+
   @override
   void initState() {
     super.initState();
   }
 
-  Widget buildListView() {
-    return ListView(
-      scrollDirection: Axis.horizontal,
-      children: List.generate(compressedImages.length, (index) {
-        //File asset = compressedImages[index];
-
-        return SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  child: Image.file(compressedImages[index],
-                      height: 100, width: 100),
-                ),
-                Text(
-                    "${compressedImages[index].absolute.readAsBytesSync().lengthInBytes / 1024}"),
-              ],
-            ),
-          ),
-        );
-      }),
-    );
-  }
-
   Widget buildInfoListView() {
-    return ListView(
-      children: [
-        SizedBox(
-          height: 600,
-          child: ListView.builder(
-            itemCount: 1,
-            itemBuilder: (context, index) {
-              // for (var item in imagesList) {
-              //RandomData data = item;
-              return SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: List.generate(imagesList.length, (index) {
-                      var img = imagesList[index]!;
-                      imgName = imagesList[index].name!;
-                      var width1 = imagesList[index].originalWidth!;
-                      var height1 = imagesList[index].originalHeight!;
-                      return Column(children: [
-                        AssetThumb(
-                          asset: img,
-                          width: 100,
-                          height: 100,
-                        ),
-                        Text("$imgName"),
-                        Text("$height1"),
-                        Text("$width1"),
-                      ]);
-                    }),
-                  ));
-              //}
-            },
-          ),
-        ),
-      ],
+    return SizedBox(
+      height: 200,
+      child: ListView.builder(
+        itemCount: resultList.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          {
+            var img = resultList[index]!;
+            imgName = resultList[index].name!;
+            var width1 = resultList[index].originalWidth!;
+            var height1 = resultList[index].originalHeight!;
+            return FutureBuilder<ByteData>(
+              future: img.getByteData(),
+              builder: (context, snapshot) {
+                // if (snapshot.connectionState ==
+                //     ConnectionState.waiting) {
+                //   return CircularProgressIndicator(); // Show a loader while waiting for image data
+                // }
+
+                // if (!snapshot.hasData) {
+                //   return Text(
+                //       'Image data not available'); // Handle the case when image data is not available
+                // }
+
+                ByteData? byteData = snapshot.data;
+                if (byteData == null) {
+                  return Text(
+                      'Image data not available'); // Handle the case when image data is null
+                }
+                imageSizekb = (byteData.lengthInBytes) / 1024;
+                return Column(children: [
+                  AssetThumb(
+                    asset: img,
+                    width: 50,
+                    height: 50,
+                  ),
+                  Text('Image Size: $imageSizekb'),
+                  Text("$imgName "),
+                  Text("Height: $height1"),
+                  Text("Width: $width1"),
+                ]);
+              },
+            );
+          }
+        },
+      ),
     );
   }
+  // Widget buildInfoListView() {
+  //   return SizedBox(
+  //     height: 200,
+  //     child: ListView.builder(
+  //       itemCount: myImagesList.length,
+  //       scrollDirection: Axis.horizontal,
+  //       itemBuilder: (context, index) {
+  //         {
+  //           return Column(children: [
+  //             AssetThumb(
+  //               asset: myImagesList[index].img,
+  //               width: 50,
+  //               height: 50,
+  //             ),
+  //             Text('Image Size: ${myImagesList[index].size}'),
+  //             Text("Height: ${myImagesList[index].height}"),
+  //             Text("Width: ${myImagesList[index].width}"),
+  //           ]);
+  //         }
+  //       },
+  //     ),
+  //   );
+  // }
 
-  Widget buildGridView() {
-    return GridView.count(
-      crossAxisCount: 3,
-      children: List.generate(imagesList.length, (index) {
-        Asset asset = imagesList[index];
-        return AssetThumb(
-          asset: asset,
-          width: 300,
-          height: 300,
-        );
-      }),
-    );
+  // Widget buildListView() {
+  //   return SizedBox(
+  //     height: 200,
+  //     child: ListView.builder(
+  //       itemCount: myComImagesList.length,
+  //       scrollDirection: Axis.horizontal,
+  //       itemBuilder: (context, index) {
+  //         {
+  //           return Column(children: [
+  //             Container(
+  //               width: 50,
+  //               height: 50,
+  //               child: myComImagesList[index].img,
+  //             ),
+  //             Text('Image Size: ${myComImagesList[index].size}'),
+  //             Text("Height: ${myComImagesList[index].height}"),
+  //             Text("Width: ${myComImagesList[index].width}"),
+  //           ]);
+  //         }
+  //       },
+  //     ),
+  //   );
+  // }
+
+  Widget buildListView() {
+    print("compressedImageList ${compressedImages.length}");
+
+    return compressedImages.isNotEmpty
+        ? ListView.builder(
+            itemCount: compressedImages.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Container(
+                      width: 50,
+                      height: 50,
+                      child: Image.file(compressedImages[index],
+                          height: 50, width: 50),
+                    ),
+                    Text(
+                        "Size: ${compressedImages[index].absolute.readAsBytesSync().lengthInBytes / 1024}"),
+                    Text("Height: List<File> compressedImages"),
+                    Text(
+                        "Width: ${Image.file(compressedImages[index]).height}"),
+                  ],
+                ),
+              );
+            },
+          )
+        : Container();
   }
 
   Future<void> loadAssets() async {
-    List<Asset> resultList = <Asset>[];
     String error = 'No Error Detected';
-
+    print("pick image");
     try {
       resultList = await MultiImagePicker.pickImages(
         maxImages: 300,
@@ -133,44 +181,62 @@ class _UploadImageScreenState extends State<UploadImageScreen> {
           selectCircleStrokeColor: "#000000",
         ),
       );
+
+      print("Image selected");
+
+      // imagesList = resultList;
+      // _error = error;
+
+      for (var index = 0; index < resultList.length; index++) {
+        //resultList.forEach((element) async {
+        var bytedata = await resultList[index].getByteData();
+        Uint8List imageData = await bytedata.buffer.asUint8List();
+        int sizeInBytes1 = imageData.lengthInBytes;
+        double sizeInKilobytes1 = sizeInBytes1 / 1024;
+        double sizeInMegabytes1 = sizeInKilobytes1 / 1024;
+        dynamic size4;
+        if (sizeInKilobytes1 <= 1024) {
+          size4 = "${sizeInKilobytes1.toStringAsFixed(2)} KB";
+        } else {
+          size4 = "${sizeInMegabytes1.toStringAsFixed(2)} MB";
+        }
+        var randomData = RandomData(
+            img: resultList[index],
+            height: resultList[index].originalHeight!,
+            width: resultList[index].originalWidth!,
+            size: size4,
+            path: resultList[index].name!);
+        myImagesList.add(randomData);
+      }
+
+      if (resultList.isNotEmpty) {
+        compressImages();
+      }
+      //});
+      setState(() {});
     } on Exception catch (e) {
       error = e.toString();
     }
-
-    if (!mounted) return;
-    setState(() {
-      imagesList = resultList;
-      _error = error;
-    });
-
-    if (imagesList.isNotEmpty) {
-      await compressImages();
-    }
-
-    print("Image selected");
   }
 
   Future<void> compressImages() async {
-    for (image3 in imagesList) {
-      ByteData byteData = await image3.getByteData(quality: 100);
-      Uint8List imageData = byteData.buffer.asUint8List();
+    print("resultlist: ${resultList.length}");
+    for (int index = 0; index < resultList.length; index++) {
+      print("index $index");
 
       print('before compressed');
-      print("Name ${image3.name}");
-      final temp = await getTemporaryDirectory();
-      BCompresspath = '${temp.path}/${image3.name}';
-      print("path $BCompresspath");
-      print("identifier ${image3.identifier}");
-      height = image3.originalHeight!;
-      width = image3.originalWidth!;
-      print("originalHeight $height");
-      print("originalWidth $width");
+      print("Name ${resultList[index].name}");
+      int height = resultList[index].originalHeight!;
+      int width = resultList[index].originalWidth!;
+      print("originalHeight $height originalWidth $width");
+      ByteData byteData = await resultList[index].getByteData();
+
+      Uint8List imageData = byteData.buffer.asUint8List();
       int sizeInBytes1 = imageData.lengthInBytes;
-      sizeInKilobytes1 = sizeInBytes1 / 1024;
+      double sizeInKilobytes1 = sizeInBytes1 / 1024;
       double sizeInMegabytes1 = sizeInKilobytes1 / 1024;
-      print('Size in bytes: $sizeInBytes1');
-      print('Size in kilobytes: $sizeInKilobytes1');
-      print('Size in megabytes: $sizeInMegabytes1');
+      print(
+          'Size in bytes: $sizeInBytes1 Size in kilobytes: $sizeInKilobytes1 Size in megabytes: $sizeInMegabytes1');
 
       var compressedImage =
           await FlutterImageCompress.compressWithList(imageData,
@@ -180,28 +246,47 @@ class _UploadImageScreenState extends State<UploadImageScreen> {
               format: CompressFormat.jpeg);
 
       var tempDir = await getTemporaryDirectory();
-      File compressedFile = File('${tempDir.path}/${image3.name}');
+      File compressedFile =
+          await File('${tempDir.path}/${resultList[index].name}');
+      print("file ${compressedFile}");
       await compressedFile.writeAsBytes(compressedImage);
-      img4 = compressedImage;
-      int sizeInBytes = compressedImage.lengthInBytes;
-      sizeInKilobytes = sizeInBytes / 1024;
-      double sizeInMegabytes = sizeInKilobytes / 1024;
-      print('After compressed Image');
-      print('Size in bytes: $sizeInBytes');
-      print('Size in kilobytes: $sizeInKilobytes');
-      print('Size in megabytes: $sizeInMegabytes');
 
+      int sizeInBytes = compressedImage.lengthInBytes;
+      double sizeInKilobytes = sizeInBytes / 1024;
+      double sizeInMegabytes = sizeInKilobytes / 1024;
+      dynamic size3;
+      if (sizeInKilobytes <= 1024) {
+        size3 = "${sizeInKilobytes.toStringAsFixed(2)} KB";
+      } else {
+        size3 = "${sizeInMegabytes.toStringAsFixed(2)} MB";
+      }
+      print('After compressed Image');
+      print(
+          'Size in bytes: $sizeInBytes Size in kilobytes: $sizeInKilobytes Size in megabytes: $sizeInMegabytes');
       var decodedImage =
           await decodeImageFromList(compressedFile.readAsBytesSync());
-      print('Height ${decodedImage.height}');
-      print('Width ${decodedImage.width}');
-      ACompresspath = compressedFile.path;
-      print(ACompresspath);
+      var ACompresspath = compressedFile.path;
+      print(
+          '\nHeight ${decodedImage.height} \nWidth ${decodedImage.width} \n$ACompresspath');
 
-      setState(() {
-        compressedImages.add(compressedFile);
-      });
+      compressedImages.add(compressedFile);
+      print(compressedImages.length);
+      for (var index = 0; index < compressedImages.length; index++) {
+        var comRandomData = CompRandomData(
+          img: Image.file(compressedImages[index], height: 50, width: 50),
+          height: decodedImage.height,
+          width: decodedImage.width,
+          size: size3,
+        );
+        // if (compressedImages.isNotEmpty) {
+        //   compressedImages.clear();
+        // }
+        myComImagesList.add(comRandomData);
+        setState(() {});
+      }
     }
+
+    setState(() {});
   }
 
   Future<void> uploadImage() async {
@@ -245,38 +330,30 @@ class _UploadImageScreenState extends State<UploadImageScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return ModalProgressHUD(
-      inAsyncCall: showSpinner,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Upload Image'),
-        ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Center(child: Text('Error: $_error')),
-              ElevatedButton(
-                onPressed: loadAssets,
-                child: const Text("Pick images"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Upload Image'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Center(child: Text('Error: $_error')),
+            ElevatedButton(
+              onPressed: loadAssets,
+              child: const Text("Pick images"),
+            ),
+            SizedBox(height: 200, child: buildInfoListView()),
+            SizedBox(height: 200, child: buildListView()),
+            Visibility(
+              visible: imagesList.isEmpty ? false : true,
+              child: ElevatedButton(
+                child: const Text("Upload"),
+                onPressed: () async {
+                  uploadImage();
+                },
               ),
-
-              SizedBox(height: 200, child: buildInfoListView()),
-              SizedBox(height: 200, child: buildListView()),
-              // Text(
-              //     'before compressed \nPath: $BCompresspath \nHeight: $height \nWidth: $width \nKB: $sizeInKilobytes1'),
-              // Text(
-              //     'After compressed \nPath: $ACompresspath \nHeight: $height \nWidth: $width \nKB: $sizeInKilobytes'),
-              Visibility(
-                visible: imagesList.isEmpty ? false : true,
-                child: ElevatedButton(
-                  child: const Text("Upload"),
-                  onPressed: () async {
-                    uploadImage();
-                  },
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
