4a97c73bce8cc74ccf2c9b796d82dd80dce3cc31
admin <pradip@adstringo.in>
2023-07-25 17:25:49 +0530

    25 july

---
+++ lib/comModel.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CompRandomData {
+  Image img;
+  int height;
+  int width;
+  dynamic size;
+
+  CompRandomData({
+    required this.img,
+    required this.height,
+    required this.width,
+    required this.size,
+  });
+}
---
+++ lib/model.dart
@@ -0,0 +1,16 @@
+import 'package:multi_image_picker/multi_image_picker.dart';
+
+class RandomData {
+  Asset img;
+  int height;
+  int width;
+  dynamic size;
+  String path;
+
+  RandomData(
+      {required this.img,
+      required this.height,
+      required this.width,
+      required this.size,
+      required this.path});
+}
